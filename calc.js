b46c5ebde5c544a2ed7c2e42152c693f702f4711
MorganSlimak <morganslimak@gmail.com>
2015-10-15 19:39:39 -0400

    made loan slider value responsive, made some layout changes, modified css

--- calc.js
+++ calc.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
 
-$( "#slider" ).slider({
+$("#slider").slider({
 	range: "min",
 	min: 0,
-	max: 50,
+	max: 50000,
+	value: 8000,
+	step: 100,
+	slide: function(event, ui) {
+        $("#loan-val").val("$" + ui.value);
+      }
 });
+
+$("#loan-val").val("$8000");
 
 
 
