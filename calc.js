a8708e61d512d5f103d7516968c86bd94581de13
MorganSlimak <morganslimak@gmail.com>
2015-10-18 13:58:54 -0400

    added calculation formulas and finished application

--- calc.js
+++ calc.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 
+var r = 10/1200;
+var pv = 8000;
+var n = 24;
+
 $("#slider").slider({
 	range: "min",
 	min: 0,
@@ -8,6 +12,8 @@ $("#slider").slider({
 	step: 100,
 	slide: function(event, ui) {
         $("#loan-val").val("$" + ui.value);
+        pv = ui.value;
+        calculate();
       }
 });
 
@@ -20,6 +26,8 @@ $("#slider2").slider({
 	value: 24,
 	slide: function(event, ui) {
         $("#time").val(ui.value + " Months (~" + Math.floor((ui.value/12)) + " Yr)");
+        n = ui.value;
+        calculate();
       }
 });
 
@@ -34,11 +42,21 @@ $("#slider3").slider({
 	value: 10,
 	slide: function(event, ui) {
         $("#apr").val(ui.value + "%");
+        r  = ui.value/1200;
+        calculate();
       }
 });
 
 $("#apr").val("10%");
 
+function calculate() {
+	var payments = (r * pv)/(1-Math.pow(1+r, -n));
+	payments = Math.round(payments * 100)/100;
+	var total = payments * n;
+	$("#payments").val("$" + payments);
+	$("#total").val("$" + total);
+}
 
+calculate();
 
 })
